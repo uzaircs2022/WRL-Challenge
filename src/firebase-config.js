e94c50323e3654d9358a43c16094833659bba0a7
uzaircs2022 <syeduzair222.sus@gmail.com>
2024-11-28 07:05:49 -0500

    Added the ENV Variables for Security

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAcIcR_C1qH9JLuOSM0-Qy4clbrjDqnWgw",
-  authDomain: "digital-world-logbook-dev.firebaseapp.com",
-  projectId: "digital-world-logbook-dev",
-  storageBucket: "digital-world-logbook-dev.appspot.com",
-  messagingSenderId: "610788060881",
-  appId: "1:610788060881:web:00c891f19c2ee69b889ff0",
-  measurementId: "G-BVM20TJDPH"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
